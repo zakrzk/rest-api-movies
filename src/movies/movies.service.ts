5263eef77509b040c48cd5b419b6fe818c0c69e3
zakrzk <bzakrzewski1@gmail.com>
2020-09-17 17:56:50 +0200

    delete redundant movieAlreadyInDb()

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,22 +1,17 @@
-import { NotFoundException, Injectable, BadRequestException } from '@nestjs/common';
+import { NotFoundException, Injectable } from '@nestjs/common';
 
 import { Movie } from './movie.model';
 import { fetchMovieData, findMovie } from './movie.dao';
-import { addMovieToDB } from '../app.db';
+import { addMovieToDB, getMoviesFromDB } from '../app.db';
 
 @Injectable()
 export class MoviesService {
-  movies: Movie[] = [];
-
   async addMovie(title: string, year: number) {
 
     const foundMovieId = await findMovie(title, year);
 
     if (!foundMovieId) {
       throw new NotFoundException('Movie not found. Check title and the year.');
-    }
-    if (this.movieAlreadyInDb(foundMovieId)) {
-      throw new BadRequestException('Movie already in the database.');
     }
 
     const newMovie = await fetchMovieData(foundMovieId).then(
@@ -33,27 +28,13 @@ export class MoviesService {
       },
     );
 
-    this.movies.push(newMovie);
+
     await addMovieToDB(newMovie);
     return newMovie;
   }
 
-
-  private movieAlreadyInDb(foundMovieId: string) {
-    // TODO: check if the movie is already in the DB, if so return 400 Bad Request
-    return false;
+  async getMovies(): Promise<Movie[]> {
+    return await getMoviesFromDB();
   }
 
-  getMovies() {
-    return [...this.movies];
-  }
-
-  // private findMovie(id: string): Movie {
-  //   const movieId = this.movies.findIndex(movie => movie.id === id);
-  //   const movie = this.movies[movieId];
-  //   if (!movie) {
-  //     throw new NotFoundException('Movie not in the DB. Please add it first.')
-  //   }
-  //   return movie;
-  // }
 }
